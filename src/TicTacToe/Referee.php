2fe16b2ef34ef91e6f722bf34b4e92db9fbe4b43
Jon Skulski <jskulski@gmail.com>
2015-04-03 11:44:15 -0700

    Extracting some methods from the referee

--- src/TicTacToe/Referee.php
+++ src/TicTacToe/Referee.php
@@ -13,16 +13,44 @@ class Referee {
    */
   public function makeCall(Move $move, array $lastMoves)
   {
-    $lastMove = $lastMoves[0];
-    if ($move->equals($lastMove)) {
+    if ($this->moveHasBeenMade($move, $lastMoves)) {
       return false;
-    }
+    };
 
-    if ($move->isX() && $lastMove->isX()) {
+    $lastMove = $lastMoves[0];
+    if ($this->playerOfMoveIsSameAsLastMove($move, $lastMove)) {
       return false;
     }
 
     return true;
+  }
+
+  /**
+   * @param Move $move
+   * @param Move[] $lastMoves
+   * @return bool
+   */
+  private function moveHasBeenMade(Move $move, array $lastMoves)
+  {
+    $lastMove = $lastMoves[0];
+    if ($move->equals($lastMove)) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * @param Move $move
+   * @param Move $lastMove
+   * @return bool
+   */
+  private function playerOfMoveIsSameAsLastMove(Move $move, Move $lastMove)
+  {
+    if ($move->isX() && $lastMove->isX()) {
+      return true;
+    }
+
+    return false;
   }
 
 }
