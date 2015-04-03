0017123cd8a785844f5270f15e518a80f3ac1f05
Jon Skulski <jskulski@gmail.com>
2015-04-03 00:10:39 -0700

    Introduce weird gameState_isOver field. That should be extracted to object soon.

--- src/TicTacToe/Game.php
+++ src/TicTacToe/Game.php
@@ -6,14 +6,18 @@ namespace JSK\TicTacToe;
 
 class Game {
 
+  /** @var  boolean */
+  private $gameState_isOver = false;
+
+
   public function makeMove()
   {
-    
+    $this->gameState_isOver = true;
   }
 
   public function isOver()
   {
-    return true;
+    return $this->gameState_isOver;
   }
 
 }
