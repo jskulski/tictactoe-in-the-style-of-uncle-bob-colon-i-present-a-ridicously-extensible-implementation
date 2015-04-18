880732694cf9752438102d7de15de4c7cc57c06e
Jon Skulski <jskulski@gmail.com>
2015-04-18 00:01:33 -0700

    Fixing build

--- test/Battleship/Game/GameStateTest.php
+++ test/Battleship/Game/GameStateTest.php
@@ -6,4 +6,10 @@ namespace JSK\Battleship\Game;
 
 class GameStateTest extends \PHPUnit_Framework_TestCase {
 
+
+  public function test_game_state_exists()
+  {
+    $target = new GameState();
+  }
+
 }
