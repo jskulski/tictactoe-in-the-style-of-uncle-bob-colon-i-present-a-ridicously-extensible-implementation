f42c3fc090208f59322abdfc9e99380b5e3b4090
Jon Skulski <jskulski@gmail.com>
2015-04-20 14:32:33 -0700

    Starting to use a table

--- src/TicTacToe/StaticWeb/index.php
+++ src/TicTacToe/StaticWeb/index.php
@@ -6,7 +6,43 @@ class StateRenderer {
 
   public function render(\JSK\TicTacToe\Game\State $state)
   {
-    $html = 'hello world';
+    $html =<<<EOF
+<table cellspacing="4">
+  <tr>
+    <td>
+      <button>Make move</button>
+    </td>
+    <td>
+      <button>Make move</button>
+    </td>
+    <td>
+      <button>Make move</button>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <button>Make move</button>
+    </td>
+    <td>
+      <button>Make move</button>
+    </td>
+    <td>
+      <button>Make move</button>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <button>Make move</button>
+    </td>
+    <td>
+      <button>Make move</button>
+    </td>
+    <td>
+      <button>Make move</button>
+    </td>
+  </tr>
+</table>
+EOF;
     return $html;
   }
 
