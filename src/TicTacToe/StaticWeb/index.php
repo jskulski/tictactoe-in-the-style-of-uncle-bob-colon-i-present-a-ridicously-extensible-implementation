dae10e9f191b732e6006af136ab9b60b371e8230
Jon Skulski <jskulski@gmail.com>
2015-04-25 13:20:10 -0700

    Using templates, hello world

--- src/TicTacToe/StaticWeb/index.php
+++ src/TicTacToe/StaticWeb/index.php
@@ -4,10 +4,13 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 
 $app = new \Slim\Slim();
 $app->get('/', function() use ($app) {
-  $stateRenderer = new StateRenderer();
-  $state = new \JSK\TicTacToe\Game\State();
-  $html = $stateRenderer->render($state);
+  $template = new League\Plates\Engine('templates');
+  $html = $template->render('board');
   echo $html;
+//  $templat = new \JSK\TicTacToe\StaticWeb\Template();
+//  $stateRenderer = new \JSK\TicTacToe\StaticWeb\StateRenderer();
+//  $state = new \JSK\TicTacToe\Game\State();
+//  $html = $stateRenderer->render($state);
 });
 
 $app->run();
---
+++ src/TicTacToe/StaticWeb/templates/board.php
@@ -0,0 +1,19 @@
+hello world
+
+<table>
+  <tr>
+    <td><button>move here</button></td>
+    <td><button>move here</button></td>
+    <td><button>move here</button></td>
+  </tr>
+  <tr>
+    <td><button>move here</button></td>
+    <td><button>move here</button></td>
+    <td><button>move here</button></td>
+  </tr>
+  <tr>
+    <td><button>move here</button></td>
+    <td><button>move here</button></td>
+    <td><button>move here</button></td>
+  </tr>
+</table>
