40a3db3707d74c246c8215a188391caf39154573
Jon Skulski <jskulski@gmail.com>
2015-04-28 12:39:45 -0700

    Refactor tests to make way for the future

--- test/TicTacToe/Game/MoveFiltererTest.php
+++ test/TicTacToe/Game/MoveFiltererTest.php
@@ -4,22 +4,35 @@
 namespace TicTacToe\Game;
 
 
+use JSK\TicTacToe\Game\Move;
 use JSK\TicTacToe\Game\MoveFilterer;
 use JSK\TicTacToe\Game\NullMove;
 use JSK\TicTacToe\Game\PlayerMove;
 
 class MoveFiltererTest extends \PHPUnit_Framework_TestCase {
 
-  public function test_given_a_list_of_moves_we_retrieve_the_top_left()
+  /** @var MoveFilterer */
+  private $target;
+  /** @var Move[] */
+  private $moveHistory;
+  /** @var Move */
+  private $topLeftMove;
+
+  public function setUp()
   {
-    $moveFilterer = new MoveFilterer();
-    $topLeftMove = PlayerMove::forO(-1, -1);
-    $moveHistory = array(
+    $this->target = new MoveFilterer();
+    $this->topLeftMove = PlayerMove::forO(-1, -1);
+    $this->moveHistory = array(
       new NullMove(),
-      $topLeftMove,
+      $this->topLeftMove,
       PlayerMove::forX(0, 0)
     );
-    $move = $moveFilterer->filter($moveHistory)->movesInTopLeft();
-    $this->assertEquals($move, $topLeftMove);
+
+  }
+
+  public function test_given_a_list_of_moves_we_retrieve_the_top_left()
+  {
+    $move = $this->target->filter($this->moveHistory)->movesInTopLeft();
+    $this->assertEquals($move, $this->topLeftMove);
   }
 }
