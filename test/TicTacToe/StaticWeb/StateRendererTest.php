5b504c9f14a85c60bcb7e25ab859ea8c8c218166
Jon Skulski <jskulski@gmail.com>
2015-04-28 11:43:35 -0700

    Removing tests that we rewrote

--- test/TicTacToe/StaticWeb/StateRendererTest.php
+++ test/TicTacToe/StaticWeb/StateRendererTest.php
@@ -35,46 +35,6 @@ class StateRendererTest extends \PHPUnit_Framework_TestCase
     $this->target = new StateRenderer($this->templateStub);
   }
 
-//  public function test_given_a_template_renderer_calls_render_on_template()
-//  {
-//    $rendered = $this->target->render(new State());
-//    $this->assertTrue($this->templateStub->rendered);
-//  }
-//
-//  public function test_given_state_without_moves_renders_custom_board()
-//  {
-//    $magicKey = 'this is my custom board';
-//    $target = new StateRenderer(new CustomTemplateStub($magicKey));
-//    $rendered = $target->render(new State());
-//    $this->assertEquals($magicKey, $rendered);
-//  }
-
-//  public function test_given_state_without_moves_renders_empty_board()
-//  {
-//    $rendered = $this->target->render(new State());
-//    $this->assertEquals('---\n---\n---', $rendered);
-//  }
-//
-//  public function test_given_state_with_move_in_center_renders_mark_in_center()
-//  {
-//    $state = new State();
-//    $state->addMoveToMoveHistory(
-//      PlayerMove::forX(0, 0)
-//    );
-//    $rendered = $this->target->render($state);
-//    $this->assertEquals('---\n-X-\n---', $rendered);
-//  }
-//
-//  public function test_given_state_with_move_in_top_right_renders_mark_in_top_right()
-//  {
-//    $state = new State();
-//    $state->addMoveToMoveHistory(
-//      PlayerMove::forX(-1, -1)
-//    );
-//    $rendered = $this->target->render($state);
-//    $this->assertEquals('X--\n---\n---', $rendered);
-//  }
-
   public function test_given_a_move_for_player_one_state_renderer_renders_our_X_template()
   {
     /** @var Move $move */
