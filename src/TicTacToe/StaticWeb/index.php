26393bdb58170abc0c72c2896279125f611a4837
Jon Skulski <jskulski@gmail.com>
2015-04-29 23:38:03 -0700

    At least let's get this into a class. Not sure what class this will become.

--- src/TicTacToe/StaticWeb/index.php
+++ src/TicTacToe/StaticWeb/index.php
@@ -2,58 +2,81 @@
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
+class SlimApplicationHandlerBatman {
+
+  public function displayGame() {
+    $template = new League\Plates\Engine('templates');
+    $moveFilterer = new \JSK\TicTacToe\Game\MoveFilterer();
+    $stateRenderer = new \JSK\TicTacToe\StaticWeb\StateRenderer($template, $moveFilterer);
+    $state = new \JSK\TicTacToe\Game\State();
+    return $stateRenderer->renderBoard($state->getMoveHistory());
+  }
+
+  public function makeMove($moveName) {
+    $move = $this->convertMoveNameParameterToMove($moveName);
+
+    $state = new \JSK\TicTacToe\Game\State();
+    $state->addMoveToMoveHistory($move);
+
+    $template = new League\Plates\Engine('templates');
+    $moveFilterer = new \JSK\TicTacToe\Game\MoveFilterer();
+    $stateRenderer = new \JSK\TicTacToe\StaticWeb\StateRenderer($template, $moveFilterer);
+    return $stateRenderer->renderBoard($state->getMoveHistory());
+  }
+
+  /**
+   * @param $moveName
+   * @return \JSK\TicTacToe\Game\PlayerMove
+   */
+  private function convertMoveNameParameterToMove($moveName)
+  {
+    switch ($moveName) {
+      case 'topLeft':
+        $move = \JSK\TicTacToe\Game\PlayerMove::forX(-1, -1);
+        break;
+      case 'topMiddle':
+        $move = \JSK\TicTacToe\Game\PlayerMove::forX(-1, 0);
+        break;
+      case 'topRight':
+        $move = \JSK\TicTacToe\Game\PlayerMove::forX(-1, 1);
+        break;
+
+      case 'middleLeft':
+        $move = \JSK\TicTacToe\Game\PlayerMove::forX(0, -1);
+        break;
+      case 'middleMiddle':
+        $move = \JSK\TicTacToe\Game\PlayerMove::forX(0, 0);
+        break;
+      case 'middleRight':
+        $move = \JSK\TicTacToe\Game\PlayerMove::forX(0, 1);
+        break;
+
+      case 'bottomLeft':
+        $move = \JSK\TicTacToe\Game\PlayerMove::forX(1, -1);
+        break;
+      case 'bottomMiddle':
+        $move = \JSK\TicTacToe\Game\PlayerMove::forX(1, 0);
+        break;
+      case 'bottomRight':
+        $move = \JSK\TicTacToe\Game\PlayerMove::forX(1, 1);
+        break;
+    }
+    return $move;
+  }
+
+}
+
 $app = new \Slim\Slim();
 $app->get('/', function() use ($app) {
-  $template = new League\Plates\Engine('templates');
-  $moveFilterer = new \JSK\TicTacToe\Game\MoveFilterer();
-  $stateRenderer = new \JSK\TicTacToe\StaticWeb\StateRenderer($template, $moveFilterer);
-  $state = new \JSK\TicTacToe\Game\State();
-  $html = $stateRenderer->renderBoard($state->getMoveHistory());
+  $handler = new SlimApplicationHandlerBatman();
+  $html = $handler->displayGame();
   echo $html;
 });
 
 $app->post('/move', function() use ($app) {
   $moveName = $app->request->params('moveName');
-
-  switch ($moveName) {
-    case 'topLeft':
-      $move = \JSK\TicTacToe\Game\PlayerMove::forX(-1, -1);
-      break;
-    case 'topMiddle':
-      $move = \JSK\TicTacToe\Game\PlayerMove::forX(-1,  0);
-      break;
-    case 'topRight':
-      $move = \JSK\TicTacToe\Game\PlayerMove::forX(-1,  1);
-      break;
-
-    case 'middleLeft':
-      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 0, -1);
-      break;
-    case 'middleMiddle':
-      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 0,  0);
-      break;
-    case 'middleRight':
-      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 0,  1);
-      break;
-
-    case 'bottomLeft':
-      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 1, -1);
-      break;
-    case 'bottomMiddle':
-      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 1,  0);
-      break;
-    case 'bottomRight':
-      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 1,  1);
-      break;
-  }
-
-  $state = new \JSK\TicTacToe\Game\State();
-  $state->addMoveToMoveHistory($move);
-
-  $template = new League\Plates\Engine('templates');
-  $moveFilterer = new \JSK\TicTacToe\Game\MoveFilterer();
-  $stateRenderer = new \JSK\TicTacToe\StaticWeb\StateRenderer($template, $moveFilterer);
-  $html = $stateRenderer->renderBoard($state->getMoveHistory());
+  $handler = new SlimApplicationHandlerBatman();
+  $html = $handler->makeMove($moveName);
   echo $html;
 });
 
