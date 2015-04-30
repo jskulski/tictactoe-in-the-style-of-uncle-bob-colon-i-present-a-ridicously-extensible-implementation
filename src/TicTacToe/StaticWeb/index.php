09b003bcc45ce3af683309c7250ec0460f35280e
Jon Skulski <jskulski@gmail.com>
2015-04-29 23:27:38 -0700

    Support all moves

--- src/TicTacToe/StaticWeb/index.php
+++ src/TicTacToe/StaticWeb/index.php
@@ -14,10 +14,41 @@ $app->get('/', function() use ($app) {
 
 $app->post('/move', function() use ($app) {
   $moveName = $app->request->params('moveName');
-  $move = \JSK\TicTacToe\Game\PlayerMove::forX(0, 0);
+
+  switch ($moveName) {
+    case 'topLeft':
+      $move = \JSK\TicTacToe\Game\PlayerMove::forX(-1, -1);
+      break;
+    case 'topMiddle':
+      $move = \JSK\TicTacToe\Game\PlayerMove::forX(-1,  0);
+      break;
+    case 'topRight':
+      $move = \JSK\TicTacToe\Game\PlayerMove::forX(-1,  1);
+      break;
+
+    case 'middleLeft':
+      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 0, -1);
+      break;
+    case 'middleMiddle':
+      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 0,  0);
+      break;
+    case 'middleRight':
+      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 0,  1);
+      break;
+
+    case 'bottomLeft':
+      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 1, -1);
+      break;
+    case 'bottomMiddle':
+      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 1,  0);
+      break;
+    case 'bottomRight':
+      $move = \JSK\TicTacToe\Game\PlayerMove::forX( 1,  1);
+      break;
+  }
+
   $state = new \JSK\TicTacToe\Game\State();
   $state->addMoveToMoveHistory($move);
-
 
   $template = new League\Plates\Engine('templates');
   $moveFilterer = new \JSK\TicTacToe\Game\MoveFilterer();
