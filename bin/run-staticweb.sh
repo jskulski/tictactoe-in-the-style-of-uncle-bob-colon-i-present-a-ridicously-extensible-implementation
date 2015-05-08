d2eaae50197706eb02deab423cf0c72df641e8a0
Jon Skulski <jskulski@gmail.com>
2015-05-07 17:16:36 -0700

    Slim needs script to be called index.php as near as I can figure out. Or everything is routed to /

--- bin/run-staticweb.sh
+++ bin/run-staticweb.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-php -S localhost:8000 "$DIR/../src/TicTacToe/StaticWeb/App.php"
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$SCRIPT_DIR/../src/TicTacToe/StaticWeb/" && php -S localhost:8000
 
